9f356f5a90346483ab08865a8875b36e1b82939f
Yasin Soliman <yasinS@users.noreply.github.com>
2018-02-03 16:02:12 +0000

    Add FeedPress, Unbounce, and UserVoice strings

--- findtakeovers.sh
+++ findtakeovers.sh
@@ -13,6 +13,9 @@ searches=(
     "Your CNAME settings"
     "The resource that you are attempting to access does not exist or you don't have the necessary permissions to view it."
     "Domain mapping upgrade for this domain not found"
+    "The feed has not been found"
+    "The requested URL / was not found on this server."
+    "This UserVoice subdomain is currently available!"
 )
 
 for str in "${searches[@]}"; do
