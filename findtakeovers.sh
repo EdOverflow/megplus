1bea9dde5f3f7e268f77283f336d1e91cc401f94
Yasin Soliman <yasinS@users.noreply.github.com>
2018-02-03 16:32:44 +0000

    Remove Unbounce string due to false-positive risk

--- findtakeovers.sh
+++ findtakeovers.sh
@@ -14,7 +14,6 @@ searches=(
     "The resource that you are attempting to access does not exist or you don't have the necessary permissions to view it."
     "Domain mapping upgrade for this domain not found"
     "The feed has not been found"
-    "The requested URL / was not found on this server."
     "This UserVoice subdomain is currently available!"
 )
 
