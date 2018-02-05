809415a01dd7d28eb0633a0f4810d0117247a8fd
EdOverflow <edoverflow@users.noreply.github.com>
2018-02-05 17:52:03 +0100

    Set up findbuckets.sh.

--- findbuckets.sh
+++ findbuckets.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 grep --color -Pri \
-  '(/|2F)?\K([\w\.\-_]+)\.(amazonaws\.com|digitaloceanspaces\.com|blob\.core\.windows\.net)(/|%2F)?([\w\.\-_]+)?' \
-  sort -u
+  '(/|2F)?\K([\w\.\-_]+)\.(amazonaws\.com|digitaloceanspaces\.com|blob\.core\.windows\.net)(/|%2F)?([\w\.\-_]+)?' out/
